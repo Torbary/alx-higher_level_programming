52e3f1cf32865f44692ce07a0af8b6a94d7d7340
Torbary <torbarey020@gmail.com>
2023-01-05 13:35:17 -0800

    prints the result of the addition of all arguments

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
-import sys
+from sys import argv
 
 # The arguments are stored in the sys.argv list
 # with the first element being the name of the script itself
 # We can skip the first element using a slice
 
-args = sys.argv[1:]
 
 # Initialize the sum to 0
 
@@ -14,7 +13,7 @@ result = 0
 
 # Iteratre through the agrs and add them to the sum
 
-for s in args:
+for s in argv[1:]:
     result += int(s)
 
 print(result)
