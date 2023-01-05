2959c14cba64f52b25d584e890a462472d7246b8
Torbary <torbarey020@gmail.com>
2023-01-05 13:06:57 -0800

    sys.argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == '__main__':
+
+    import sys
+    argv = sys.argv[1:]
+    num_args = len(argv)
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument:")
+    else:
+        print(f"{num_args} arguments:")
+    for i , arg in enumerate(argv, start=1):
+        print(f"{i}: {arg}")
