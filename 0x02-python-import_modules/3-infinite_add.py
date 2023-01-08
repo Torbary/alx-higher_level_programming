ecc2c77c3f8a9da0507d2ade90011bc48610a1d5
Torbary <torbarey020@gmail.com>
2023-01-08 02:58:02 -0800

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,17 +1,7 @@
 #!/usr/bin/python3
-
-from sys import argv
-
-# The arguments are stored in the sys.argv list
-# with the first element being the name of the script itself
-# We can skip the first element using a slice
-
-
-# Initialize the sum to 0
-
-result = 0
-
-# Iteratre through the agrs and add them to the sum
+if __name__ == "__main__":
+    from sys import argv
+    result = 0
 
 for s in argv[1:]:
     result += int(s)
