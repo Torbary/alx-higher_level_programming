98e7d56272bbc3a203eab44068aea4a70a813381
Torbary <torbarey020@gmail.com>
2023-01-08 03:28:51 -0800

    5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,14 +1,4 @@
 #!/usr/bin/python3
-import variable_load_5
-
-
-def print_a():
-    print(variable_load_5.a)
-
-
-def main():
-    print_a()
-
-
 if __name__ == "__main__":
-    main()
+    from variable_load_5 import a
+    print(a)
