2782d31cfa966180694894743c622dd3759a4f85
Torbary <torbarey020@gmail.com>
2023-01-11 15:00:09 -0800

    dict.keys()

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,4 +1,7 @@
 #!/usr/bin/pyhton3
 
 def number_keys(a_dictionary):
-    return len(a_dictionary)
+    nb_keys = 0
+    for a_dictionary in a_dictionary.keys():
+        nb_keys += 1
+    return nb_keys
