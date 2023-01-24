d9e0e978f6afefb828c853bcf4658180e0a13e98
Torbary <torbarey020@gmail.com>
2023-01-24 03:39:49 -0800

    magic cal

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -8,7 +8,7 @@ def magic_calculation(a, b):
             if i > a:
                 raise Exception('Too far')
             result += a ** b / i
-        except ValueError:
+        except:
             result = b + a
             break
     return result
