b77dc2ec495bbe96493e539fe52f4b701b5b4cd7
Torbary <torbarey020@gmail.com>
2023-01-31 01:09:06 -0800

    Simple Rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+"""this is module '0-rectangle' that defines a class Rectangle"""
+
+
+class Rectangle:
+    """ this class defines a rectangle"""
+    def __repr__(self):
+        pass
