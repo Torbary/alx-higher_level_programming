1dc6f1f037965e6c7acb717d534f85ea39b83942
Torbary <torbarey020@gmail.com>
2023-01-31 07:00:53 -0800

    How many instances

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -48,7 +48,6 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         self.__height = value
 
-
     def area(self):
         """returns the area of the rectangle"""
         return self.__width * self.__height
