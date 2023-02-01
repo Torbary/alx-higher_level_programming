5369715ed7d9fb2e8767cf44771377e4df82b045
Torbary <torbarey020@gmail.com>
2023-01-31 22:07:10 -0800

    Test Driven Development

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -7,11 +7,11 @@ import numpy as np
 def lazy_matrix_mul(m_a, m_b):
     """
     Multiply two matrices using NumPy
-    
+
     Parameters:
     m_a (numpy.ndarray): First matrix to be multiplied
     m_b (numpy.ndarray): Second matrix to be multiplied
-    
+
     Returns:
     numpy.ndarray: Resultant matrix after multiplying two matrices
     """
