343105caabe431b78c1f07602cb6d20bcd74055f
Torbary <torbarey020@gmail.com>
2023-02-06 14:27:35 -0800

    IMprove geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" class BaseGeometry based on 5-based_geometry.py
+"""
+
+
+class BaseGeometry:
+    """ a class with public attr area"""
+    pass
+    def area(self):
+        """raises an Exception"""
+        raise Exception("area() is not implemented")
