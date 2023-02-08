1ec951dafe73bef413ed5b6efdb8860251196b06
Torbary <torbarey020@gmail.com>
2023-02-08 02:24:05 -0800

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,15 +3,12 @@
     contains 5-save_to_json_file module
     contains 6-load_from_json_file module
 """
-
+from sys import argv
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
-
-import json
-from sys import argv
 
 filename = "add_item.json"
 
