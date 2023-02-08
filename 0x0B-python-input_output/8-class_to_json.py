b0f2961161cbc1eeac362bea2f9d3868ae26edd7
Torbary <torbarey020@gmail.com>
2023-02-08 02:49:02 -0800

    Class to JSON

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+    contains the function class_to_json(obj)
+"""
+
+def class_to_json(obj):
+    """
+        function that returns thr dict descripition
+        with simple data structure JSON serialization
+        of an object:
+    """
+    return obj.__dict__
+
+
