67467510d3a84b0c921b2407a56adf83ee9854e0
Torbary <torbarey020@gmail.com>
2023-02-08 02:21:16 -0800

    Load, add, save

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+    contains 5-save_to_json_file module
+    contains 6-load_from_json_file module
+"""
+
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+
+import json
+from sys import argv
+
+filename = "add_item.json"
+
+try:
+    json_list = load_from_json_file(filename)
+except FileNotFoundError:
+    json_list = []
+
+for arg in argv[1:]:
+    json_list.append(arg)
+save_to_json_file(json_list, filename)
