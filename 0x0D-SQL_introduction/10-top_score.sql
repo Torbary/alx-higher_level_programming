37cf1ee8df3b34d99a5e70e60285dc6516b23297
Torbary <torbarey020@gmail.com>
2023-03-15 03:12:07 -0700

    list top_score

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,6 @@
+-- lists all records of the table second_table of the db htbn_0c_0
+-- should display both the score and the name(in this order)
+-- should be orderd by score(top first)
+
+SELECT score, name FROM hbtn_0c_0.second_table ORDER BY score DESC;
+
