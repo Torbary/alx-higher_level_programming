27fd4c9594b7db3441ec3f7dc37eb0ee87013e04
Torbary <torbarey020@gmail.com>
2023-03-17 09:33:26 -0700

    display NULL if a show doesnt have a genre

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,11 @@
+-- lists all shows contained in the database hbtn_0d_tvshows
+-- Each record should didplay: tv_shows.title - tv_show_genre_id
+-- Results must be sorted in ascending ordeer by tv_shows.title and 
+-- tv_show_genres.genre_id
+
+SELECT  tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+
