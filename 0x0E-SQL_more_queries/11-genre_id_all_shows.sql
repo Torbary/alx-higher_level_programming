85c6fdd8cca04f8081d68990a33149d6d846a664
Torbary <torbarey020@gmail.com>
2023-03-17 09:41:25 -0700

    whitespace reoved from the second line

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -3,7 +3,7 @@
 -- Results must be sorted in ascending ordeer by tv_shows.title and 
 -- tv_show_genres.genre_id
 
-SELECT  tv_shows.title, tv_show_genres.genre_id
+SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id IS NULL
