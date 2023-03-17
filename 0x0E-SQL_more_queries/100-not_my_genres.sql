8dc6283001ffc288b1a2c9f6b56d9f3ba7da16d8
Torbary <torbarey020@gmail.com>
2023-03-17 13:07:51 -0700

    list all genres not linked to the show DEXTER

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,3 +1,8 @@
+-- list all genres not linked to the show Dexter
+-- The tv_shows table contains only one record where title = Dexter
+-- (but the id can be different)
+-- Each record should display: tv_genres.name
+-- Results must be sorted in ascending order by the genre name
 SELECT `name` FROM `tv_genres`
 WHERE `id` NOT IN (
 	SELECT `genre_id` FROM `tv_show_genres`
