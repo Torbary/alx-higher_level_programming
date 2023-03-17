b4ab4e75de24168557fbd524dbead1c9954b61a2
Torbary <torbarey020@gmail.com>
2023-03-17 13:22:14 -0700

    comment added

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -1,3 +1,7 @@
+-- The tv_genres table contains only one record where name = Comedy
+-- (but the id can be different)
+-- Each record should display: tv_shows.title
+-- Results must be sorted in ascending order by the show title
 SELECT title
 FROM tv_shows
 WHERE id NOT IN (
