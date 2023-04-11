fce7ac3ac0dd0bff16a4e458b896f3d6be575528
Torbary <torbarey020@gmail.com>
2023-04-11 15:28:14 +0100

    factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const factorial = (num) => {
+  if (isNaN(num)) {
+    return 1;
+  }
+
+  if (num === 0 || num === 1) {
+    return 1;
+  }
+  return num * factorial(num - 1);
+};
+
+const num = process.argv[2];
+const result = factorial(parseInt(num));
+console.log(result);
