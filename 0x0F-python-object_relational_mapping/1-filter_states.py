d23b28183540d1c555688a68bf2206029fcc2178
Torbary <torbarey020@gmail.com>
2023-04-16 00:41:59 +0100

    filter only N%

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""module for 0-select_state.sql"""
+
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+
+    conn = MySQLdb.connect(host='localhost', port=3306, user=username,
+                           passwd=password, db=dbname, charset='utf8')
+
+    cur = conn.cursor()
+
+    cur.execute("SELECT DISTINCT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    conn.close()
