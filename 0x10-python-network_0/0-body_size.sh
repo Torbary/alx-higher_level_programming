081c5e70e64667cf62f599ade4a6768d60de58f9
Torbary <torbarey020@gmail.com>
2023-04-27 23:28:19 +0100

    0-body_size.sh

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
 # Get the URL from the command line arguments
-url="$1"
-
-# Send a request to the URL using curl and store the output in a variable
-response=$(curl -sI "$url")
-
-# Get the content length of the response using grep and awk
-content_length=$(echo "$response" | grep -i "Content-Length:" | awk '{print $2}')
-
-# Display the content length in bytes
-echo "$content_length"
+curl -s "$1" | wc -c
