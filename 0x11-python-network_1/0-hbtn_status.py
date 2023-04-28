2b29b2f7bcc739580b1177aefdbf0c1c72a4e801
Torbary <torbarey020@gmail.com>
2023-04-28 10:26:55 +0100

    feches a given URL

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,14 +1,16 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """python script that fetches the gioven URL using urllib package"""
 
 
 import urllib.request
 """import urllib and access request"""
 
-with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    html = response.read()
 
-print("Body response:")
-print("\t- type: {}".format(type(html)))
-print("\t- content: {}".format(html))
-print("\t- utf8 content: {}".format(html.decode("utf-8")))
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
+        html = resp.read()
+
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode('utf-8')))
