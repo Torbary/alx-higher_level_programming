08b9b7a6966f3bf6fff5c13d3545a8c3058e35f1
Torbary <torbarey020@gmail.com>
2023-05-24 05:08:38 +0100

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,14 +1,5 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
-const filePath = process.argv[2]; // Read the file path from command line arguments
-const content = process.argv[3]; // Read the string to write from command line arguments
-
-fs.writeFile(filePath, content, 'utf8', function (error) {
-  if (error) {
-    console.error(error);
-    return;
-  }
-  console.log('File has been written.');
+fs.writeFile(process.argv[2], process.argv[3], error => {
+  if (error) console.log(error);
 });
