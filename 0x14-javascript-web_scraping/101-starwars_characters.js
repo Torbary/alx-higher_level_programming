8e1ab0092f37ce85f5b699b2000719fe509e9ee1
Torbary <torbarey020@gmail.com>
2023-05-24 07:50:13 +0100

    101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,42 +1,20 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-const movieId = process.argv[2]; // Read the movie ID from command line arguments
-const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`; // Construct the API URL
-
-request.get(apiUrl, function (error, response, body) {
-  if (error) {
-    console.error(error);
-    return;
-  }
-
-  if (response.statusCode === 200) {
-    const movie = JSON.parse(body);
-    const characterUrls = movie.characters;
-
-    // Function to fetch and print character name
-    const fetchCharacterName = (url) => {
-      request.get(url, function (charError, charResponse, charBody) {
-        if (charError) {
-          console.error(charError);
-          return;
-        }
-
-        if (charResponse.statusCode === 200) {
-          const character = JSON.parse(charBody);
-          console.log(character.name);
-        } else {
-          console.log(`Unable to fetch character: ${url}`);
-        }
-      });
-    };
-
-    // Fetch and print character names in the same order as characterUrls
-    characterUrls.forEach((url) => {
-      fetchCharacterName(url);
-    });
-  } else {
-    console.log('Unable to fetch movie');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    const characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
