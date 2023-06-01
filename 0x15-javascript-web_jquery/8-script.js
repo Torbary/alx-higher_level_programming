2902ccb5140fd6b4ebe7eca016b8b86687b8f4eb
Torbary <torbarey020@gmail.com>
2023-06-01 02:48:54 +0100

    with JQuery,fetches and lists the title for all movies

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,16 @@
+(function ($) {
+  $(document).ready(function () {
+    $.ajax({
+      url: 'https://swapi-api.alx-tools.com/api/films/?format=json',
+      method: 'GET',
+      success: function (data) {
+        $.each(data.results, function (index, movie) {
+          $('#list_movies').append('<li>' + movie.title + '</li>');
+        });
+      },
+      error: function () {
+        $('#list_movies').append('<li>Error fetching movies</li>');
+      }
+    });
+  });
+})(jQuery);
