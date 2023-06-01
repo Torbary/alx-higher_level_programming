49fde040125265307f0d798c310d7f85ab6997d0
Torbary <torbarey020@gmail.com>
2023-06-01 02:10:47 +0100

    with JQuery to addClass to element

--- 0x15-javascript-web_jquery/3-script.js
+++ 0x15-javascript-web_jquery/3-script.js
@@ -1,3 +1,7 @@
-$('DIV#red_header').click(() => {
-  $('HEADER').addClass('red');
-});
+(function ($) {
+  $(document).ready(function () {
+    $('#red_header').click(function () {
+      $('header').css('color', '#FF0000');
+    });
+  });
+})(jQuery);
