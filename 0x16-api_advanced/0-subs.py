c7894ddefaa82ab7f5344f646e4191a28b4cad3e
Torbary <torbarey020@gmail.com>
2023-06-06 16:49:35 +0100

    queries the Reddit API and returns the number of subscribers for a given subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API and returns the number of subscribers
+    for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers for the subreddit.
+        Returns 0 if the subreddit is invalid.
+    """
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    user_agent = "torbarey"
+
+    headers = {"User-Agent": user_agent}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"]["subscribers"]
+    else:
+        return 0
